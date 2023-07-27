3f3d92381e6262210fa9c8f41aae0160b7489228
Mkqupy <121451774+Mkqupy@users.noreply.github.com>
2023-07-27 19:47:18 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// 1
+let result = 5+5+'5';
+console.log(result);
+console.log(typeof result);
+
+// 2
+let email = "mariyakalinichenko1411@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+// 3
+let my = "My";
+let name = "name";
+let is = "is";
+
+let fullName = my +" "+ name +" "+ is;
+
+fullName += " Viktor";
+
+console.log(fullName);
+
+// 4
+let userName = "Олександра";
+let payment = 300;
+
+alert("Дякуємо, " + userName + "! До сплати " + payment + " гривень.");
